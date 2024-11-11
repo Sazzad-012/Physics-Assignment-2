a0f2b6ae5eb0eb564d06a954171665a5e68cc8e0
Sazzad Rashid Chowdhury <175148215+Sazzad-012@users.noreply.github.com>
2024-11-12 01:49:39 +0600

    Problem-2

---
+++ Rashid.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    double ratio = 1.0 / 0.707;//sin(45degree)= 0.707
+    printf("The ratio of the slit width to the wavelength is: %.2f\n", ratio);
+    return 0;
+}
+
