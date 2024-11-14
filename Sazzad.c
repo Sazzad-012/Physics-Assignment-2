765d369512aba618fd49d0427fe621677bc74ba2
Sazzad Rashid Chowdhury <175148215+Sazzad-012@users.noreply.github.com>
2024-11-14 21:22:54 +0600

    Update Sazzad.c

--- Sazzad.c
+++ Sazzad.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
     double wavelength, slit_separation, angle;
     printf("Enter wavelength (in nm)= ");
     scanf("%lf", &wavelength);
     if (wavelength < 380 || wavelength > 750)
-        {
+    {
         printf("Out of the range. Please enter a valid number.\n");
         return 1;
-        }
+    }
     printf("Enter slit separation (in um)= ");
     scanf("%lf", &slit_separation);
     printf("Enter angle (in degrees)= ");
     scanf("%lf", &angle);
-    int m = (int)((slit_separation * 1000 * angle * 3.14159 / 180 / wavelength) + 0.5);
+    double sin_theta= sin(angle * 3.14159 / 180);
+    int m = (int)((slit_separation * 1000 * sin_theta/ wavelength) + 0.5);
     printf("%d-th order maxima\n", m);
     return 0;
 }
+
+
+
